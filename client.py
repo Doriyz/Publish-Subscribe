63f2c0e64f6802464bef7e0042129dfacc842a5f
MaysionZhang <319474628@qq.com>
2022-10-20 23:44:50 +0800

    [VERSION1.1] Basic functions, delete on time automatically

--- client.py
+++ client.py
@@ -6,86 +6,49 @@
 @Contact :   zhangmx67@mail2.sysu.edu.cn
 '''
 
-
+import datetime
 import grpc
 import ps_pb2_grpc as pb2_grpc
 import ps_pb2 as pb2
 
 FORMAT = 'utf-8'
 
+
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         client = pb2_grpc.psStub(channel)
         while True:
-            operation = input("Enter operation: pub, sub or exit\n")
+            print()
+            print('[HELP] Enter operation: pub, sub or exit.')
+            operation = input("[INPUT] ")
             if operation == "pub":
-                topic = input("Enter topic: \n")
+                print('[HELP] Enter topic and content.')
+                topic = input("[TOPIC] ")
                 # topic = bytes(topic, FORMAT)
-                content = input("Enter content: \n")
+                content = input("[CONTENT] ")
                 # content = bytes(content, FORMAT)
                 requestMessage = pb2.PublishRequest()
                 requestMessage.topic = topic
                 requestMessage.content = content
                 response = client.publish(requestMessage)
-                print(response)
+                if response.success == 1:
+                    print('[PUBLISH] Publish success.')
             elif operation == "sub":
-                topic = input("Enter topic: \n")
+                print('[HELP] Enter topic.')
+                topic = input("[TOPIC] ")
                 requestMessage = pb2.SubscribeRequest(topic=topic)
                 response = client.subscribe(requestMessage)
                 if response.success == 1:
-                    print(f'the content is {response.content}')
+                    print('[SUBSCRIBE] Subscribe success.')
+                    print(f'[CONTENT] {response.content}')
                 else:
-                    print('no such topic')
+                    print('[ERROR] No such topic')
             elif operation == "exit":
+                print('[EXIT] Byebye ~')
                 break
             else:
-                print("Invalid operation")
+                print("[ERROR] Invalid operation")
         
 
 if __name__ == '__main__':
     run()
-
-'''
-
-class Client(object):
-
-    def __init__(self):
-        self.host = 'localhost'
-        self.server_port = 50051
-
-        # instantiate a channel
-        self.channel = grpc.insecure_channel(
-            '{}:{}'.format(self.host, self.server_port))
-
-        # bind the client and the server
-        self.stub = pb2_grpc.psStub(self.channel)
-
-
-    def subscribe(self, requestMessage):
-        return self.stub.subscribe(requestMessage)
-
-    def publish(self, requesMessage):
-        return self.stub.publish(requesMessage)
-
-if __name__ == '__main__':
-    client = Client()
-    while True:
-        operation = input("Enter operation: pub, sub or exit\n")
-        if operation == "pub":
-            topic = input("Enter topic: \n").encode(FORMAT)
-            content = input("Enter content: \n").encode(FORMAT)
-            requestMessage = pb2.PublishRequest(topic=topic, content=content)
-            response = client.publish(requestMessage)
-            print(response)
-        elif operation == "sub":
-            topic = input("Enter topic: \n")
-            requestMessage = pb2.SubscribeRequest(topic=topic)
-            response = client.subscribe(requestMessage)
-            print(response)
-        elif operation == "exit":
-            break
-        else:
-            print("Invalid operation")
-    
-
-    '''
--- server.py
+++ server.py
@@ -6,7 +6,7 @@
 @Contact :   zhangmx67@mail2.sysu.edu.cn
 '''
 
-
+import json
 import grpc
 from concurrent import futures
 import datetime
@@ -15,20 +15,49 @@ import ps_pb2 as pb2
 
 # use a dictionary to store the messages
 # the key is the topic and the value is content
-message_dict = {} 
+
+
+# def getTimestamp():
+#     dt = datetime.datetime.now()
+#     return pb2.timestamp(year = dt.year, month = dt.month, day = dt.day, hour = dt.hour, minute = dt.minute, second = dt.second)
+
+
 
 class psService(pb2_grpc.psServicer):
+    
+    message_dict = {} 
+    time_dict = {}
 
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, message_dict, time_dict, **kwargs):
+        self.message_dict = message_dict
+        self.time_dict = time_dict
 
     def publish(self, request, content):
         """
         publish a message to a topic
         """
+        # before publish, delete the out-time message
+        # using a list comprehension to make a list of the keys to be deleted
+        delete = []
+        for key in self.time_dict:
+            time = self.time_dict[key]
+            dt = datetime.datetime.strptime(time[0:26], "%Y-%m-%d %H:%M:%S") 
+            if dt + datetime.timedelta(seconds=60) < datetime.datetime.now():
+                delete.append(key)
+        for key in delete:
+            print(f'[DELETE] Delete topic {key}')
+            self.message_dict.pop(key)
+            self.time_dict.pop(key)
         if len(request.content) != 0:
             print(f'[PUBLISH] Add topic {request.topic}')
-            message_dict[request.topic] = request.content
+            self.message_dict[request.topic] = request.content
+            # it does not work to store datetime to a json file
+            self.time_dict[request.topic] = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+            # write the message to a locol file to restore
+            with open('message.json', 'w') as json_file:
+                json.dump(self.message_dict, json_file)
+            with open('time.json', 'w') as json_file:
+                json.dump(self.time_dict, json_file)
             return pb2.PublishResponse(success = 1) 
         else:
             return pb2.publishResponse(success = 0)
@@ -38,35 +67,27 @@ class psService(pb2_grpc.psServicer):
         """
         subscribe a topic and get the message
         """
-        print(f'[SUBSCRIBE] Subscribe topic {request.topic}')
         while True:
-            if request.topic in message_dict:
-                return pb2.SubscribeResponse(success = 1 , content = message_dict[request.topic])
+            if request.topic in self.message_dict:
+                print(f'[SUBSCRIBE] Subscribe topic {request.topic}')
+                return pb2.SubscribeResponse(success = 1 , content = self.message_dict[request.topic])
             else:
                 return pb2.SubscribeResponse(success = 0, content = '')
 
-'''     
-class UnaryService(pb2_grpc.UnaryServicer):
-
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def GetServerResponse(self, request, context):
-
-        # get the string from the incoming request
-        message1 = request.message1
-        message2 = request.message2
-        result = f'{message1}  {message2}'
-        result = {'message': result, 'received': True}
-
-        return pb2.MessageResponse(**result)
-
-'''
 
 def serve():
     print('[START] Server is working now.')
+    # firsly load the message dictionary
+    json_file = open('message.json', 'r')
+    message_dict = json.load(json_file)
+    json_file.close()
+    
+    json_file = open('time.json', 'r')
+    time_dict = json.load(json_file)
+    json_file.close()
+
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    pb2_grpc.add_psServicer_to_server(psService(), server)
+    pb2_grpc.add_psServicer_to_server(psService(message_dict, time_dict ), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
